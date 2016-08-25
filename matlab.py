7586272cdfb3720acd3073a6da7cc0233fa91c61
Luke <mugapedia@gmail.com>
2016-08-25 16:50:19 +0300

    (feature) Adds parsing for different inputs types
    This feature adds parsing to determine the kind of input that was
    provided by the user in the terminal. It helps to differentiate the
    different kinds of expressions such as simple variable assignments
    such as a=5, array declrations such as a = [1, 2, 4], and matrix
    creation expressions such as a = [1, 2, 3; 4, 5, 6; 7, 8, 9]

--- matlab.py
+++ matlab.py
@@ -20,8 +20,22 @@ class Matlab(cmd.Cmd):
             parts = str(args).split('=')
             identifier = parts[0].strip()
             array = parts[1]
-            Matlab.class_vars[identifier] = array
-            print(Matlab.class_vars[identifier])
+            # Check if array contains semicolons, to determine
+            # if it's a matrix
+            if array.find(';') != -1:
+                array = array.strip('[] ')
+                array = array.replace(',', ' ')
+                array = array.replace(';', '\n')
+                Matlab.class_vars[identifier] = array
+                print(Matlab.class_vars[identifier])
+            elif array.isalpha():
+                # Case of simple variable assignment
+                Matlab.class_vars[identifier] = array
+                print(Matlab.class_vars[identifier])
+            else:
+                # Normal array
+                Matlab.class_vars[identifier] = array
+                print(Matlab.class_vars[identifier])
         else:
             if args in Matlab.class_vars:
                 print(Matlab.class_vars[args])
@@ -37,9 +51,37 @@ class Matlab(cmd.Cmd):
         '''
         vector = "0\n"
         vector *= int(num_rows)
-        print(vector)
+        Matlab.class_vars[vector] = result
+        print(Matlab.class_vars[vector])
+        
+    def compute_sum(self, a, b):
+        '''
+            Compute sum of two numbers
+        '''
+        return int(a) + int(b)
+        
+    def do_add_number_to_matrix(self, number):
+        '''
+            Add a number to a matrix. Convert the matrix
+            into a single long list then add the number to
+            each list element before turning it back into
+            a matrix once again
+        '''
+#        matrix = Matlab.class_vars[matrix]
+#        r = list(self.compute_sum(number, matrix))
+        print(number)
+        
+    def do_greet(self, name):
+        '''
+            Greet name 
+        '''    
+        if name != None:
+            print(name)
+        else:
+            print('Hey you folks')
     
     def do_EOF(self, line):
+        print('Exiting Application, Bye')
         return True
         
 if __name__ == '__main__':
